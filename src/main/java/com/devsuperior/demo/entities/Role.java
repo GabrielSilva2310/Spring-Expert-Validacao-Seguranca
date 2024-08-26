d5e19c70f3547448661ad47e60ddb3d75ee8119a
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2024-08-26 02:25:07 -0300

    Checklist Spring Security

--- src/main/java/com/devsuperior/demo/entities/Role.java
+++ src/main/java/com/devsuperior/demo/entities/Role.java
@@ -2,6 +2,7 @@ package com.devsuperior.demo.entities;
 
 import java.util.Objects;
 
+import org.springframework.security.core.GrantedAuthority;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,9 +11,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
 
 	
 	@Id
@@ -40,7 +42,7 @@ public class Role {
 		this.id = id;
 	}
 
-
+	@Override
 	public String getAuthority() {
 		return authority;
 	}
--- src/main/java/com/devsuperior/demo/entities/User.java
+++ src/main/java/com/devsuperior/demo/entities/User.java
@@ -6,6 +6,10 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.devsuperior.demo.projections.UserDetailsProjection;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -19,9 +23,10 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User implements UserDetails {
 
 	
 	@Id
@@ -49,6 +54,15 @@ public class User {
 		this.name = name;
 		this.email = email;
 		this.password = password;
+	}
+	
+	public User(List<UserDetailsProjection> projections) {
+		this.email=projections.get(0).getUsername();
+		this.password=projections.get(0).getPassword();
+		for(UserDetailsProjection projection : projections) {
+			addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+		}
+		
 	}
 	
 	
@@ -116,6 +130,39 @@ public class User {
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+
 	}
 
 
